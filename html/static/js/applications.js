e84e0bc325a6dab384708bd7c067ad0885170c80
fabionm <fabio.n.m.h@gmail.com>
2017-06-21 08:29:15 -0700

    Ajustes menu mobile e tela Minhas compras  com adicicao do btn confirmar pagamento

--- html/static/js/applications.js
+++ html/static/js/applications.js
@@ -61,12 +61,6 @@
         });
     }
 
-    // Closes the Responsive Menu on Menu Item Click
-    $('.navbar-collapse ul li a').click(function()
-    {
-            $('.navbar-toggle:visible').click();
-    });
-
     if($('body.index-home #mainNav').affix)
     {
         // Offset for Main Navigation
